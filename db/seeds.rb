73be7f87e8408b313dabda4038f21aeed0b156f1
DENEUBOURG Cedric <deneubourg.cedric@thedigitalfab.com>
2020-06-03 21:34:33 +0200

    Add Item Migration & Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+## Generate Items
+10.times do
+  Item.create!(
+    title: Faker::Creature::Cat.registry,
+    description: Faker::Lorem.sentence(word_count: 10, supplemental: false, random_words_to_add: 10), #between 10 and 20 words
+    price: Faker::Number.decimal(l_digits: 2),
+    image_url: "https://pauline.dev/chatons/#{Faker::Number.between(from: 1, to: 20)}.jpg"
+  )
+end
